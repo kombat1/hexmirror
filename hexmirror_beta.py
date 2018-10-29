bc27a53eb6250737398282e5e747b5cb9c24c7f5
kombat <kolyakovalev2000@mail.ru>
2018-10-29 18:11:13 +0600

    Update hexmirror_beta.py

--- hexmirror_beta.py
+++ hexmirror_beta.py
@@ -1,7 +1,7 @@
-a = '646с726а77206а6с6с6548'
-hexcode = ''.join(reversed([r'\x' + a[i:i+2] for i in range(0, len(a), 2)]))
-print(hexcode)
+import sys
 
-b = "\x48\x65\x6с\x6с\x6а\x20\x77\x6а\x72\x6с\x64" # Hex код который скрипт сгенерировал 
+a = sys.argv[1]
 
-v = "\x48\x65\x6c\x6c\x6a\x20\x77\x6a\x72\x6c\x64" # Hex код который я ввел вручную 
+hexcode = ''.join(reversed([r'\x' + a[i:i+2] for i in range(0, len(a), 2)]));print("\n"+hexcode)
+
+
