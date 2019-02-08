88a2cbdd7fb501253d581577c2a28c40d32554fb
kombat <kolyakovalev2000@mail.ru>
2019-02-08 23:30:38 +0900

    Create hexmirror.py

---
+++ hexmirror v1/hexmirror.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2
+
+import argparse
+
+args = argparse.ArgumentParser()
+args.add_argument('hex',help='hex or path',metavar='')
+
+
+group = args.add_mutually_exclusive_group()
+group.add_argument('-b','--byte',help='Byte ;)',action="store_true")
+group.add_argument('-P','--Path',help='Path ;)',action="store_true")
+
+
+
+args = args.parse_args()
+
+
+
+def HexMirror(hex_code):
+	a = hex_code
+	hexcode = ''.join(reversed([r'\x' + a[i:i+2] for i in range(0, len(a), 2)]));print(hexcode)
+
+def HexPath(path):
+	rev = path[::-1]
+	return rev.encode('hex')
+
+
+def main():
+	if args.byte:
+		HexMirror(args.hex)
+	if args.Path:
+		print "0x" + HexPath(args.hex)	
+
+
+if __name__ == '__main__':
+	main()		
